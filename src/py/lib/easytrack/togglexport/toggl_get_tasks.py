329ce7191adfcef128d1a327a62971dcfe25bfe4
Krzysztof Stanisławek <krzysztof.stanislawek@beacon.io>
2021-05-11 10:58:40 +0200

    fix bug with toggl backoffs

--- src/py/lib/easytrack/togglexport/toggl_get_tasks.py
+++ src/py/lib/easytrack/togglexport/toggl_get_tasks.py
@@ -1,6 +1,9 @@
 import os
+import logging
+import time
 from dataclasses import dataclass
 from typing import List
+import urllib
 
 from toggl.TogglPy import Endpoints, Toggl
 
@@ -20,17 +23,34 @@ def toggl_get_tasks(token=None) -> List[TglTask]:
     toggl.setAPIKey(token)
 
     workspaces = []
-    for e in toggl.getWorkspaces():
+    for e in _retry(lambda: toggl.getWorkspaces()):
         workspaces.append((e["id"], e["name"]))
 
     projects = []
     for wid, wname in workspaces:
-        for e in toggl.request(Endpoints.WORKSPACES + f"/{wid}/projects") or []:
+        for e in _retry(lambda: toggl.request(Endpoints.WORKSPACES + f"/{wid}/projects")) or []:
             projects.append((e["id"], e["name"]))
 
     ptasks = []
     for pid, pname in projects:
-        for e in toggl.getProjectTasks(pid) or [{"id": 0, "name": ""}]:
+        for e in _retry(lambda: toggl.getProjectTasks(pid)) or [{"id": 0, "name": ""}]:
             ptasks.append(TglTask(pid, pname, e["id"], e["name"]))
 
     return ptasks
+
+
+def _retry(func, max_retries=10, wait_seconds=0.1):
+    retries = 0
+    while True:
+        try:
+            res = func()
+            if retries:
+                logging.debug(f'Success on retry {retries}')
+            return res
+        except urllib.error.HTTPError:
+            logging.debug("Repeating url request")
+            retries += 1
+            if retries == max_retries:
+                raise
+            else:
+                time.sleep(wait_seconds)
